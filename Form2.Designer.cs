682a77d9884dc3a2bf4b639ff8d38ad301f16f47
ZNS <collobertalexandre95@gmail.com>
2026-07-18 14:32:45 +0200

    dd

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -57,65 +57,80 @@
             "Model",
             "Knife",
             "Upgrade"});
-            this.listBox1.Location = new System.Drawing.Point(102, 173);
+            this.listBox1.Location = new System.Drawing.Point(110, 273);
             this.listBox1.Name = "listBox1";
             this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBox1.Size = new System.Drawing.Size(130, 134);
+            this.listBox1.Size = new System.Drawing.Size(155, 121);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(44, 95);
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(218, 87);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(35, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Title:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(85, 95);
+            this.textBox2.Location = new System.Drawing.Point(71, 12);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(216, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(85, 121);
+            this.textBox3.Location = new System.Drawing.Point(71, 50);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(216, 20);
             this.textBox3.TabIndex = 5;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 121);
+            this.textBox4.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(262, 114);
             this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(67, 20);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = "Description:";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(85, 147);
+            this.textBox5.Location = new System.Drawing.Point(110, 247);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(179, 20);
+            this.textBox5.Size = new System.Drawing.Size(155, 20);
             this.textBox5.TabIndex = 7;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(12, 147);
+            this.textBox6.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox6.ForeColor = System.Drawing.Color.White;
+            this.textBox6.Location = new System.Drawing.Point(72, 247);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(67, 20);
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(32, 20);
             this.textBox6.TabIndex = 6;
             this.textBox6.Text = "Icon:";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(270, 144);
+            this.button1.Location = new System.Drawing.Point(271, 247);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.Size = new System.Drawing.Size(31, 20);
             this.button1.TabIndex = 8;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
@@ -123,7 +138,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(189, 380);
+            this.button2.Location = new System.Drawing.Point(190, 400);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
@@ -133,7 +148,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(108, 380);
+            this.button3.Location = new System.Drawing.Point(110, 400);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
@@ -145,7 +160,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.ClientSize = new System.Drawing.Size(384, 461);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -158,6 +173,7 @@
             this.Controls.Add(this.listBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Publish Mod";
             this.ResumeLayout(false);
             this.PerformLayout();
